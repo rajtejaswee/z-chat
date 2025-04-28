e8721ecc2ec2f51be6343263a3888608ed598d1f
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-04-28 15:13:58 +0530

    Completed

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user.model.js';
-import { generateToken } from '../utils/jwtTokenGenerator.js';
+import { generateToken } from '../lib/utils.js';
 import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,18 +1,42 @@
+import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/database.js";
-import { app } from "./app.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-connectDB()
-    .then(() => {
-        app.on("error", (error) => {
-            console.log("ERROR:", error);
-            throw error;
-        })
-        app.listen(process.env.PORT || 80000, () => {
-            console.log(`Server is running at port : ${process.env.PORT}`)
-        })
-    })
-    .catch((error) => {
-        console.log("MongoDb connection failed !!!", error)
-})
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
